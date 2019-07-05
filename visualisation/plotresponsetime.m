8eae3c447603ad84ccd3c9f8c8f2a90595b25191
Joshua Arnold <joshua.arnold1@uqconnect.edu.au>
2019-07-05 13:58:10 +1000

    Added moving average

--- visualisation/plotresponsetime.m
+++ visualisation/plotresponsetime.m
@@ -5,12 +5,12 @@ for n = 1 : numel(neurons)
 
     neuron = neurons(n);
     
-    plot(out.spike_time_trace(out.spike_time_trace(:, 2) == neuron, 1), mod(out.spike_time_trace(out.spike_time_trace(:, 2) == neuron, 1), 500), '.')
+    plot(out.spike_time_trace(out.spike_time_trace(:, 2) == neuron, 1), mod(out.spike_time_trace(out.spike_time_trace(:, 2) == neuron, 1), 500) + 0.1 * n - 0.1, '.')
     hold on
     plot(out.spike_time_trace(out.spike_time_trace(:, 2) == neuron, 1), movmean(mod(out.spike_time_trace(out.spike_time_trace(:, 2) == neuron, 1), 500), 20));
     
-    leg{n} = ['N', num2str(neuron)]; 
-    leg{n} = ['N', num2str(neuron), ' movmean'];
+    leg{n*2 - 1} = ['N', num2str(neuron)]; 
+    leg{n * 2} = ['N', num2str(neuron), ' movmean'];
     
 end
 %plot(out.spike_time_trace(out.spike_time_trace(:, 2) == 5, 1), mod(out.spike_time_trace(out.spike_time_trace(:, 2) == 5, 1), 500), '.')
