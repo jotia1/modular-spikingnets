deeed208a6cb36f4295e661b65caf9a3ce5de40a
Joshua Arnold <joshua.arnold1@uqconnect.edu.au>
2019-07-07 10:57:22 +1000

    condition1 done

--- experiments/wright2012replication/condition1.m
+++ experiments/wright2012replication/condition1.m
@@ -1,19 +1,43 @@
 function [ net, out ] = condition1()
 %% CONDITION1 - Replication of wright, wiles 2012 Condition 1
 %   See paper for additional details.
+%
+%   NOTE: If you just look at the raster plot of this experiment it looks
+%   good. It is important to also consider the output response time (which
+%   paints a grim picture (response time increasing, learning is diverging
+%   for second pattern?). Wright, Wiles (2012) describes condition1 as only
+%   being 60 seconds (as replicated here) but fig2 which is described as if
+%   it is condition1 last for 300 seconds and the supervision times etc.
+%   are much longer (which will likely allow learning to be properly
+%   cemented). See the matlab script fig2.m for an attempt at replication
+%   of figure2 from the paper. 
 
 rng(1); 
 net = getCond1Net();
 net.rand_seed = -1;
 
 out = spikingnet(net);
-plot3inp(out);
-calcAccuracy(net, out);
+%plot3inp(out);
+figure;
+subplot(2, 2, 1);
+plottrace(out.delayst, [4]);
+title('delays trace');
+
+subplot(2, 2, 2);
+plottrace(out.vart, [4]);
+title('variance trace');
+
+subplot(2, 2, 3);
+plottrace(out.vt, [4]);
+title('voltage');
+
+subplot(2, 2, 4);
+rasterspiketimes(out.spike_time_trace, 3, 1);
+title('Neuron raster plot (compare with reponse time plot)');
 
 figure;
-rasterspiketimes(out.spike_time_trace, 3, 2);
-
-disp('break');
+plotresponsetime(net, out, [4]);
+title('Neuron 4s response time');
 
 end
 
@@ -27,33 +51,34 @@ net.N = sum(net.group_sizes);
 net.rand_seed = 1;
 
 net.delays = zeros(net.N);
-net.delays(1:3, 4:5) = 5;
+net.delays(1:3, 4) = 5;
 net.variance = zeros(net.N);
-net.variance(1:3, 4:5) = 2;
+net.variance(1:3, 4) = 2;
 net.w = zeros(net.N);
-net.w(1:3, 4:5) = 1;
+net.w(1:3, 4) = 1;
 
-net.fgi = 75;
-net.nu = 0.06;
-net.nv = 0.02;
-net.a1 = 3;%-33.2;
-net.a2 = 2; %35.6;
-net.b1 = 5; %24.9;
-net.b2 = 5; %82.3;
+net.fgi = 13;
+net.nu = 0.03;
+net.nv = 0.01;
+net.a1 = 3;
+net.a2 = 2;
+net.b1 = 5;
+net.b2 = 5; 
 net.variance_min = 0.1;
 net.variance_max = 10;
 net.neuron_tau = 20;
 net.delay_max = 15;
 
 net.v_rest = -65;
-net.v_reset = -70;
-net.v_thres = -55;
+net.v_reset = -65;
+net.v_thres = 35;
 net.w_max = 10;
 net.taupre = 20;
 net.taupost = 20;
-net.Apost = 0;%0.1;
-net.Apre = 0;%-0.1;
+net.Apost = 0;
+net.Apre = 0;
 
+net.use_izhikevich = true;
 net.fixed_integrals = false;
 net.dynamic_threshold = false;
 net.thres_rise = 10; %[mV]
@@ -65,15 +90,6 @@ net.seq_freq_ms = 500;
 net.supervised_seconds = 15;
 net.supervising = true;
 net.lateral_inhibition = [];
- 
-%[net.inp, net.ts] = createTwoPatternInput(net.seq, fliplr(net.seq), net.seq_freq_ms, net.sim_time_sec, net.supervised_seconds, 13);
-% [p1inp, p1ts] = createTwoPatternInput(net.seq, fliplr(net.seq), net.seq_freq_ms, 120, 60, 13);
-% [p2inp, p2ts] = createTwoPatternInput(fliplr(net.seq), net.seq, net.seq_freq_ms, 30, 0, 13);
-% [p3inp, p3ts] = createTwoPatternInput(fliplr(net.seq), net.seq, net.seq_freq_ms, 120, 60, 13);
-% [p4inp, p4ts] = createTwoPatternInput(net.seq, fliplr(net.seq), net.seq_freq_ms, 230, 0, 13);
-% 
-% net.inp = [p1inp, p2inp, p3inp, p4inp];
-% net.ts = [p1ts, p2ts + (120 * 1000), p3ts + (150 * 1000), p4ts + (270 * 1000)];
 
 [ inp1, ts1 ] = createInput(net.seq, net.seq_freq_ms, 25, net.supervised_seconds, 13);
 [ inp2, ts2 ] = createInput(fliplr(net.seq), net.seq_freq_ms, 5, 0, 13);
