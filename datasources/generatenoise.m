84a2bc102aa47b9823333d0bfb3a4390bbf14b0d
Joshua Arnold <joshua.arnold1@uqconnect.edu.au>
2019-08-01 23:01:16 +1000

    Noise is fored to be expected value

--- datasources/generatenoise.m
+++ datasources/generatenoise.m
@@ -4,14 +4,6 @@ function [ inp, ts ] = generatenoise( N, freq, length_ms )
 %   Assume 1000ms of noise if length not provided. Frequency is in Hz
 %   assumes 5 Hz if not provided.
 %
-%   NOTE:
-%   Duplicates are removed at the end meaning if there are lots of clashes
-%   (caused by high frequency in a small period) the statistics may be 
-%   quite off. 
-%
-%   TODO
-%       - Replace removed items
-%
 %   Example usage:
 %       [ inp, ts ] = generatenoise( 10, 2, 1000 )
 
@@ -23,16 +15,26 @@ if ~exist('length_ms', 'var') || isempty(length_ms)
     length_ms = 1000;
 end
 
-
 length_sec = length_ms / 1000;
 num_spikes = ceil(freq * length_sec * N);
 
-inp = randi([1, N], num_spikes, 1);
-ts = randi([1, length_ms], num_spikes, 1);
+inp = [];
+ts = [];
+diff = num_spikes;
 
-data = unique([ts, inp], 'rows');
-inp = data(:, 2);
-ts = data(:, 1);
+do = true;
+while do
+    
+    inp = [inp, randi([1, N], 1, diff)];
+    ts = [ts, randi([1, length_ms], 1, diff)];
+
+    data = unique([ts', inp'], 'rows');
+    
+    inp = data(:, 2)';
+    ts = data(:, 1)';
+    diff = num_spikes - numel(inp);
+    
+    do = diff ~= 0;   % If num_spikes ~= numel(inp)
 
 end
 
