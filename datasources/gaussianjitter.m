f8ea6d79c207c41d60c0326d8a736ec00543947c
Joshua Arnold <joshua.arnold1@uqconnect.edu.au>
2019-08-09 11:34:49 +1000

    Added jitter modifier

---
+++ datasources/gaussianjitter.m
@@ -0,0 +1,15 @@
+function [inp , ts ] = gaussianjitter(inp, ts, jitter)
+%% GAUSSIANJITTER - Designed to implement jitter from Masquelier paper
+%   Add Gaussian noise to a signal with mean 0 and standard deviation
+%   jitter
+%
+%   Parameters
+%       inp, ts - spike train 
+%       jitter - integer number of ms noise to add
+%
+%   Usage: 
+%       [inp , ts ] = gaussianjitter(inp, ts, 1)
+
+ts = ts + round(rand(size(ts))) * jitter;
+
+end
